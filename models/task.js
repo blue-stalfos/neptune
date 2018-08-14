540f25ed821ca05382403416423dd34c4b3c6c0a
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2018-08-14 12:49:23 -0400

    Close #6 - Created preliminary tasks schema

---
+++ models/task.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const taskSchema = new Schema({
+	label: String,
+	priority: Number,
+	location: String,
+	creator: String,
+	assignee: String,
+	dateDue: Date,
+	subTask: ObjectId //This should probably be an array of task objects
+}, {
+	timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const Task = mongoose.model("Task", userSchema);
+module.exports = Task;
